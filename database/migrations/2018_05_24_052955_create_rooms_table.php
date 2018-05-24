df0c48a475551e12da9b8ae4d3cd2365d33778b7
Arush Pal Singh <arushsingh.ds@incedoinc.com>
2018-05-24 11:12:18 +0530

    add template

---
+++ database/migrations/2018_05_24_052955_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('room_no');
+            $table->integer('floor_no')->nullable();
+            $table->string('room_type');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rooms');
+    }
+}

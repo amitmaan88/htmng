36037eb97a218611fb4e3999e031940e7646c12f
Arush Pal Singh <arushsingh.ds@incedoinc.com>
2018-06-04 15:32:54 +0530

    New Design

--- database/migrations/2018_05_28_063955_user_type.php
+++ database/migrations/2018_05_28_063955_user_type.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class UserType extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_type', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('user_type_id');
+            $table->string('type',15);
             $table->enum('status', ['1', '0'])->default('1');
-            $table->rememberToken();
-            $table->timestamps();            
+            $table->timestamps();
+            $table->index(['type']);
         });
     }
 
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('user_type');
     }
 }
